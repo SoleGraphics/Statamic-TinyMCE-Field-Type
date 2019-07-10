aa87a2006ccdcb64204ee1919769ac821bf84f4c
Michael <pixelrobin@hotmail.com>
2019-07-10 09:48:56 -0700

    fix(fieldtype): iframe going blank in set on drag

--- resources/assets/src/tinymce.js
+++ resources/assets/src/tinymce.js
@@ -16,8 +16,8 @@ Vue.component('tiny_mce_wysiwyg-fieldtype', {
     mixins: [Fieldtype],
 
     template: `
-    <div>
-        <textarea class="sg_tinymce_instance" v-model="data"></textarea>
+    <div v-el:tinycontainer>
+        <textarea v-el:tinymce class="sg_tinymce_instance" v-model="data"></textarea>
     </div>
     `,
 
@@ -28,34 +28,80 @@ Vue.component('tiny_mce_wysiwyg-fieldtype', {
     },
 
     beforeDestroy() {
-        this.editor.destroy();
+        this.destroyMCE();
     },
 
     methods: {
         updateContent(content) {
             this.data = content;
+        },
+
+        initMCE() {
+            const mceConfig = window.TinyMceWysiwygConfig || {};
+
+            tinymce.init({
+                target: this.$els.tinymce,
+                ...mceConfig,
+                init_instance_callback: (editor) => {
+                    this.editor = editor;
+                    
+                    /*
+                        This fixes a bug where dragging a set with the TinyMCE
+                        editor in it but not dropping it in a new location
+                        causes the contents of the TinyMCE iframe to break,
+                        making the editor unresponsive.
+
+                        Basically manually check every dom manipulation to see
+                        if the iframe was in it and if it was, reinitialize
+                        TinyMCE.
+
+                        It's not ideal, but it works...
+                    */
+                    const observer = new MutationObserver(mutations => {
+                        const container = this.$els.tinycontainer;
+                        const iframe = container.querySelector('iframe');
+                        let done = false;
+
+                        for (const mutation of mutations) {
+                            for (const node of mutation.addedNodes) {
+                                if (node.contains(iframe)) {
+                                    this.destroyMCE();
+                                    observer.disconnect();
+                                    this.initMCE();
+                                    
+                                    done = true;
+                                    break;
+                                }
+                            }
+
+                            if (done) break;
+                        }
+                    });
+
+                    observer.observe(document.documentElement, {
+                        childList: true,
+                        subtree: true
+                    });
+
+                    editor.on('KeyUp', (e) => {
+                        this.updateContent(editor.getContent());
+                    });
+                    editor.on('Change', (e) => {
+                        this.updateContent(editor.getContent());
+                    });
+
+                    // Set content to statamic data model
+                    editor.setContent(this.data || '');
+                }
+            });
+        },
+
+        destroyMCE() {
+            this.editor.destroy();
         }
     },
 
     ready() {
-        const mceConfig = window.TinyMceWysiwygConfig || {};
-
-        tinymce.init({
-            selector: 'textarea.sg_tinymce_instance',
-            ...mceConfig,
-            init_instance_callback: (editor) => {
-                this.editor = editor;
-
-                editor.on('KeyUp', (e) => {
-                    this.updateContent(editor.getContent());
-                });
-                editor.on('Change', (e) => {
-                    this.updateContent(editor.getContent());
-                });
-
-                // Set content to statamic data model
-                editor.setContent(this.data);
-            }
-        });
+        this.initMCE();
     }
 });
